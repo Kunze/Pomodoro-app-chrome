c177d3e56bea75b8b59c40b4c5d3eb9e81af6aeb
Murilo Kunze <murilokunze@hotmail.com>
2014-08-18 10:59:15 -0300

    removed unused variables

--- scripts/background.js
+++ scripts/background.js
@@ -1,8 +1,5 @@
 (function(window) {
 	"use strict";
 	
-	var deskbell = document.getElementById("deskbell");
-	var crank = document.getElementById("crank");
-	
 	window.timer = new Timer(60 * 25);
 }(window));
