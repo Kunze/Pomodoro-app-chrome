e253b1945ac5570257cb5e5d0f4f1454936bf362
murilo kunze <murilokunze@hotmail.com>
2016-03-02 09:39:52 -0300

    fixed end call

--- scripts/timer.js
+++ scripts/timer.js
@@ -1,99 +1,86 @@
-(function(window) {
-	"use strict";
-	
-	var ONE_SECOND = 1000;
-	
-	function Timer()
-	{
-		this.deskbell = new Audio("sounds/deskbell.wav");
-		this.crank = new Audio("sounds/crank.wav");
-		this.isRunning = false;
-		this.secondsIntervalCounter = function() { };
-		this._start = function() { };
-		this._secondChange = function() { };
-		this._end = function() { };
-		this._stop = function() { };
-	}
+(function (window) {
+    "use strict";
 
-	Timer.prototype.onStart = function(callback) { 
-		this._start = callback;
-		
-		return this;
-	};
-		
-	Timer.prototype.onSecondChange = function(callback) { 
-		this._secondChange = callback;
-		
-		return this;
-	};
-		
-	Timer.prototype.onEnd = function(callback) { 
-		this._end = callback;
-		
-		return this;
-	};
-		
-	Timer.prototype.onStop = function(callback) { 
-		this._stop = callback;
-		
-		return this;
-	};
-	
-	Timer.prototype.start = function(seconds)
-	{
-		if (timer.isRunning) {
-			return;
-		}
-		
-		if (!(this.seconds = +seconds)) {
-			return console.error("seconds");
-		}
-		
-		this.isRunning = true;
-		this.crank.play();
-		
-		var self = this;
-		this.secondsIntervalCounter = setInterval(function() {
-			if (!(--self.seconds))
-			{
-				end.call(self);
-			}
-			
-			self._secondChange();
-		}, ONE_SECOND);
-		
-		this._start();
-	}
+    var ONE_SECOND = 1000;
 
-	Timer.prototype.stop = function()
-	{
-		clearInterval(this.secondsIntervalCounter);
-		
-		this.isRunning = false;
-		this._stop();
-	}
-	
-	Timer.prototype.getCurrentTime = function()
-	{
-		var minutes = parseInt(this.seconds / 60);
-		var seconds = this.seconds % 60;
-		
-		if (seconds === 60) {
-			seconds = "00";
-		}
-		else if (seconds.toString().length === 1) {
-			seconds = "0" + seconds;
-		}
-		
-		return minutes + ":" + seconds;
-	}
+    function Timer() {
+        this.deskbell = new Audio("sounds/deskbell.wav");
+        this.crank = new Audio("sounds/crank.wav");
+        this.isRunning = false;
+        this.secondsIntervalCounter = function () { };
+        this._start = function () { };
+        this._secondChange = function () { };
+        this._end = function () { };
+        this._stop = function () { };
+    }
 
-	function end()
-	{
-		this._end();
-		this._stop();
-		this.deskbell.play();
-	}
-	
-	window.Timer = Timer;
-}(window));
+    Timer.prototype.onStart = function (callback) {
+        this._start = callback;
+
+        return this;
+    };
+
+    Timer.prototype.onSecondChange = function (callback) {
+        this._secondChange = callback;
+
+        return this;
+    };
+
+    Timer.prototype.onStop = function (callback) {
+        this._stop = callback;
+
+        return this;
+    };
+
+    Timer.prototype.start = function (seconds) {
+        if (this.isRunning) {
+            return;
+        }
+
+        if (!(this.seconds = +seconds)) {
+            return console.error("seconds");
+        }
+
+        this.isRunning = true;
+        this.crank.play();
+
+        var self = this;
+        this.secondsIntervalCounter = setInterval(function () {
+            if (!(--self.seconds)) {
+                self.end();
+            }
+
+            self._secondChange();
+        }, ONE_SECOND);
+
+        this._start();
+    }
+
+    Timer.prototype.stop = function () {
+        clearInterval(this.secondsIntervalCounter);
+
+        this.isRunning = false;
+        this._stop();
+    }
+
+    Timer.prototype.getCurrentTime = function () {
+        var minutes = parseInt(this.seconds / 60);
+        var seconds = this.seconds % 60;
+
+        if (seconds === 60) {
+            seconds = "00";
+        }
+        else if (seconds.toString().length === 1) {
+            seconds = "0" + seconds;
+        }
+
+        return minutes + ":" + seconds;
+    }
+
+    Timer.prototype.end = function () {
+        this.stop();
+        this.deskbell.play();
+    }
+
+    window.Timer = Timer;
+} (window));
